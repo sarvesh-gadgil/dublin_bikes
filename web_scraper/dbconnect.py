e914ca56a12c7cf31dc5fc4fd72bf55ee3f6d12d
sarvesh-gadgil <60133683+sarvesh-gadgil@users.noreply.github.com>
2020-02-20 19:10:39 +0000

    Changed the db config

--- web_scraper/dbconnect.py
+++ web_scraper/dbconnect.py
@@ -1,12 +1,10 @@
 import pymysql
 import requests
 
-# host = "dublin-bikes.c96ersz2ktrh.us-east-1.rds.amazonaws.com"
-host = "localhost"
+host = "dublin-bikes.c96ersz2ktrh.us-east-1.rds.amazonaws.com"
 dbname = "dublin_bikes"
 user = "root"
-# password = "dublin_bikes_root"
-password = "Test@123"
+password = "dublin_bikes_root"
 
 
 def get_db_connection():
